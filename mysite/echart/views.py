4b3c2cadecca6341e1e4ef1ae2f76981f5061674
Tina <1216448265@qq.com>
2016-09-26 19:17:34 +0800

    modify echart app, display system load

--- mysite/echart/views.py
+++ mysite/echart/views.py
@@ -11,14 +11,21 @@ def index(request):
     return render(request, 'echart/index.html', {'u1':u1 ,'u2':u2, 'u3':u3 })
 
 def cpu(request):
-    cmd = "iostat 1 2 |egrep -v [a-Z]|egrep [0-9]|tail -1"
-    result = os.popen(cmd).readline().split()
-    num = [float(f) for f in result]
-    return render(request, 'echart/cpu.html', {'list':num })
+    return render(request, 'echart/cpu.html')
 
 def cpu_info(request):
     cmd = "iostat 1 2 |egrep -v [a-Z]|egrep [0-9]|tail -1"
     result = os.popen(cmd).readline().split()
-    num = [float(f) for f in result]
-    output = json.dumps(num)
+    iostat = [float(f) for f in result]
+    output = json.dumps(iostat)
+    return HttpResponse(output)
+
+def load(request):
+    return render(request, 'echart/load.html')
+
+def load_info(request):
+    cmd = "uptime|awk -F : '{print $5}'|tr -d ','"
+    result = os.popen(cmd).readline().split()
+    load = [float(f) for f in result]
+    output = json.dumps(load)
     return HttpResponse(output)
